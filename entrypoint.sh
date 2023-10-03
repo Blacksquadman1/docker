40d6323364ee3628b1e0a00e7e82769c05af91de
Blacksquadman1 <142269714+Blacksquadman1@users.noreply.github.com>
2023-10-03 20:58:46 +0200

    teast

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
-export VSCODE_PROXY_URI=https://vscode.gamingbaeren.de:{{port}}
+export VSCODE_PROXY_URI=https://195.20.227.48:{{port}}
 # Print current Python version
 python --version
 
