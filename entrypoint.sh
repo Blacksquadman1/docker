acd9474cc6de79ae6f1155f54ac82cba829cef30
Blacksquadman1 <142269714+Blacksquadman1@users.noreply.github.com>
2023-10-05 20:13:48 +0200

    asfedgadsfasf

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
-export VSCODE_PROXY_URI=http://195.20.227.48:{{port}}
+export VSCODE_PROXY_URI=""
 # Print current Python version
 python --version
 
